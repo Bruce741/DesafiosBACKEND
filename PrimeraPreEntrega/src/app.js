5b5a85a30646d3af5cae911406416bf2709ebd77
Bruno <lopezbruno12319@gmail.com>
2023-12-19 16:46:34 -0300

    Fix2

--- PrimeraPreEntrega/src/app.js
+++ PrimeraPreEntrega/src/app.js
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cartsRouter from "./routes/carts.routes.js";
 import productsRoutes from "./routes/products.routes.js";
 
 const PORT = 8080;
 const app = express();
+
+app.use(express.json());
+app.use(urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.use("/api/products", productsRoutes);
 app.use("/api/carts", cartsRouter);
