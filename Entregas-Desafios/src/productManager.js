f195de549db194760fb8a708e6b36bca0f370370
Bruno <lopezbruno12319@gmail.com>
2023-12-26 15:36:25 -0300

    productManagerFix

--- Entregas-Desafios/src/productManager.js
+++ Entregas-Desafios/src/productManager.js
@@ -8,28 +8,50 @@ class ProductManager {
 
   static id = 0;
 
-  addProduct = async (title, description, price, thumbnail, code, stock, available, category) => {
-    ProductManager.id++;
-    let newProduct = {
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock,
-      available: available,
-      category: category,
-      id: ProductManager.id,
-    };
-
-    this.products.push(newProduct);
-
-    await fs.promises.writeFile(this.path, JSON.stringify(this.products));
+  addProduct = async (
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock,
+    available,
+    category
+  ) => {
+    try {
+      ProductManager.id++;
+      const existingProducts = await this.getProducts();
+      let newProduct = {
+        title: title,
+        description: description,
+        price: price,
+        thumbnail: thumbnail,
+        code: code,
+        stock: stock,
+        available: available,
+        category: category,
+        id: ProductManager.id,
+      };
+      existingProducts.push(newProduct);
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products));
+    } catch (error) {
+      console.error("Error al agregar el producto:", error);
+      throw error;
+    }
   };
 
   getProducts = async () => {
-    let productosLeidos = await fs.promises.readFile(this.path, "utf-8");
-    return JSON.parse(productosLeidos);
+    try {
+      let productosLeidos = await fs.promises.readFile(this.path, "utf-8");
+      if (!productosLeidos.trim()) {
+        return [];
+      }
+      return JSON.parse(productosLeidos);
+    } catch (error) {
+      console.error('Error al leer el archivo JSON:', error);
+      throw error;
+    }
+    
   };
 
   getProductsById = async (id) => {
@@ -57,9 +79,15 @@ class ProductManager {
   };
 
   deleteProduct = async (id) => {
-    let productos = await this.getProducts();
-    let productosBorrados = productos.filter((producto) => producto.id != +id);
-    await fs.promises.writeFile(this.path, JSON.stringify(productosBorrados));
+    try {
+      let productos = await this.getProducts();
+      let productosBorrados = productos.filter((producto) => producto.id != +id);
+      await fs.promises.writeFile(this.path, JSON.stringify(productosBorrados));
+    } catch (error) {
+      console.error('Error al eliminar el producto:', error);
+      throw error;
+    }
+   
   };
 }
 
